41964e84cae1085023d3e3f7edcdf24ddbeb86a8
Md Abu Ahsan Basir <maab.career@gmail.com>
2019-09-08 03:20:47 +0600

    Fixed str_slug and load resources

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -5,13 +5,35 @@
  */
 if( !function_exists('slug') ) {
 	function slug($string) {
-	return str_slug($string);
-}
+		// replace non letter or digits by -
+	  	$string = preg_replace('~[^\pL\d]+~u', '-', $string);
+
+	  	// transliterate
+	  	$string = iconv('utf-8', 'us-ascii//TRANSLIT', $string);
+
+	  	// remove unwanted characters
+	  	$string = preg_replace('~[^-\w]+~', '', $string);
+
+	  	// trim
+	  	$string = trim($string, '-');
+
+	  	// remove duplicate -
+	  	$string = preg_replace('~-+~', '-', $string);
+
+	  	// lowercase
+	  	$string = strtolower($string);
+
+	  	if (empty($string)) {
+	   		return 'n-a';
+	  	}
+
+	  	return $string;
+	}
 }
 if( !function_exists('str_spliter') ) {
 	function str_spliter( $str, $pattern = '/[\s,.|]/' ) {
-	return preg_split( $pattern, $str);
-}
+		return preg_split( $pattern, $str);
+	}
 }
 if( !function_exists('permission_asset') ) {
 	function permission_asset( $path ) {
